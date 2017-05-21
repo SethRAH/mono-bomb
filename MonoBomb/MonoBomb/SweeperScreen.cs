cf3f8e00f4d8b9320012cade7e5679037045c5c3
SethRAH <srahare@gmail.com>
2017-05-21 15:33:57 -0400

    Added adjacency counts
    Added functionality to calculate adjacencies (needs optimizing).
    Added display logic to show adjacency on grid.
    Still no real functionality logic

--- MonoBomb/MonoBomb/SweeperScreen.cs
+++ MonoBomb/MonoBomb/SweeperScreen.cs
@@ -17,6 +17,7 @@ namespace MonoBomb
         SpriteBatch spriteBatch;
         private Texture2D bombTexture, unclickedTileTexture;
         private Color bgColor;
+        private SpriteFont boardFont;
 
         public Color BGColor { get { return this.bgColor; } }
 
@@ -30,7 +31,7 @@ namespace MonoBomb
         {
             spriteBatch.Begin();
 
-            gameboard.Draw(spriteBatch, bombTexture, unclickedTileTexture);
+            gameboard.Draw(spriteBatch, bombTexture, unclickedTileTexture, boardFont);
 
             spriteBatch.End();
         }
@@ -38,10 +39,11 @@ namespace MonoBomb
         public void Init()
         {
             var gameBounds = new Rectangle(0, 0, game.Window.ClientBounds.Width, game.Window.ClientBounds.Height);
-            gameboard = new GameBoard(15,10,32,(gameBounds.Width / 2) - 240 , (gameBounds.Height / 2) - 160, 10);
+            gameboard = new GameBoard(15,10,32,(gameBounds.Width / 2) - 240 , (gameBounds.Height / 2) - 160, 8);
             spriteBatch = new SpriteBatch(game.GraphicsDevice);
             bombTexture = game.Content.Load<Texture2D>(@"Skull_Bomb");
             unclickedTileTexture = game.Content.Load<Texture2D>(@"Unclicked_Tile");
+            boardFont = game.Content.Load<SpriteFont>(@"board-font");
         }
 
         public IGameScreen Update(GameTime gameTime)
@@ -55,9 +57,11 @@ namespace MonoBomb
 
     public class GameBoard
     {
+        private readonly int xPadding = 8, yPadding = 8;
         int height, width, bombRatio, tileSize, top, left;
         bool[] isUncovered;
         bool[] isBomb;
+        int[] adjacency;
 
         public int pixHeight { get { return height * tileSize; } }
         public int pixWidth { get { return width * tileSize; } }
@@ -84,7 +88,7 @@ namespace MonoBomb
             initBoard();
         }
 
-        public void Draw(SpriteBatch spriteBatch, Texture2D bombTexture, Texture2D UnclickedTileTexture)
+        public void Draw(SpriteBatch spriteBatch, Texture2D bombTexture, Texture2D UnclickedTileTexture, SpriteFont font)
         {
             int size = height * width;
             for(int i = 0; i < size; i++)
@@ -97,6 +101,10 @@ namespace MonoBomb
                 {
                     spriteBatch.Draw(bombTexture, location, Color.White * 1.0f);
                 }
+                else if(adjacency[i] > 0)
+                {
+                    spriteBatch.DrawString(font, adjacency[i].ToString(), location + new Vector2(xPadding, yPadding), Color.White * 0.8f);
+                }
             }
         }
 
@@ -106,10 +114,43 @@ namespace MonoBomb
             int size = height * width;
             isUncovered = new bool[size];
             isBomb = new bool[size];
+            adjacency = new int[size];
             for(int i = 0; i < size; i++)
             {
                 isUncovered[i] = false;
                 isBomb[i] = random.Next(bombRatio - 1) == 0;
+            }
+
+            //After bombs are set, loop again for adjacency
+            for(int i = 0; i < size; i++)
+            {
+                int count = 0;
+                var coord = coordinates(i);
+                //NW
+                if (coord.Item1 - 1 > -1 && coord.Item2 - 1 > -1 && isBomb[index(coord.Item1 - 1, coord.Item2 - 1)]) count++;
+
+                //N
+                if (coord.Item1 > -1 && coord.Item2 - 1 > -1 && isBomb[index(coord.Item1, coord.Item2 - 1)]) count++;
+
+                //NE
+                if (coord.Item1 + 1 < width - 1 && coord.Item2 - 1 > -1 && isBomb[index(coord.Item1 + 1, coord.Item2 - 1)]) count++;
+
+                //E
+                if (coord.Item1 + 1 < width - 1 && coord.Item2  > -1 && isBomb[index(coord.Item1 + 1, coord.Item2)]) count++;
+
+                //SE
+                if (coord.Item1 + 1 < width && coord.Item2 + 1 < height && isBomb[index(coord.Item1 + 1, coord.Item2 + 1)]) count++;
+
+                //S
+                if (coord.Item1 > -1 && coord.Item2 + 1 < height && isBomb[index(coord.Item1, coord.Item2 + 1)]) count++;
+
+                //SW
+                if (coord.Item1 - 1 > -1 && coord.Item2 + 1 < height && isBomb[index(coord.Item1 -1 , coord.Item2 + 1)]) count++;
+
+                //W
+                if (coord.Item1 - 1 > -1 && coord.Item2 > -1 && isBomb[index(coord.Item1 - 1, coord.Item2)]) count++;
+
+                adjacency[i] = count;
             }
         }
 
